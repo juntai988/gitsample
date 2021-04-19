9e817c727f5dce962df1f2fb03029cdc661a0b24
juntai988 <priceless.shunta4.14@icloud.com>
2021-04-19 15:52:05 +0900

    Initial Commit

---
+++ gitsample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitsample
+//
+//  Created by 高橋俊太 on 2021/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitsample/gitsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitsampleApp.swift
+//  gitsample
+//
+//  Created by 高橋俊太 on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct gitsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
